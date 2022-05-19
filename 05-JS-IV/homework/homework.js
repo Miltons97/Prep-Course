e15263e928dce4f5bcc9cb216ac9a74fea5d936d
Miltons97 <milton.sosa1901@gmail.com>
2022-05-19 17:01:12 -0300

    Empezamos JSIV

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -6,6 +6,16 @@ function crearGato(nombre, edad) {
   // Agrega un método (funcion) llamado "meow" que devuelva el string "Meow!"
   // Devuelve el objeto
   // Tu código:
+   
+  let Gato = {
+    nombre: "nombre",
+    edad : "edad"
+  }
+    llorar = function (){
+     console.log("Meow!")
+  
+
+  }
 }
 
 
@@ -14,6 +24,7 @@ function agregarPropiedad(objeto, property) {
   // Devuelve el objeto
   // NOTA: El nombre de la propiedad no es "propiedad", el nombre es el valor del argumento llamado "property" (una cadena/string)
   // Tu código:
+
 }
 
 function invocarMetodo(objeto, metodo) {
